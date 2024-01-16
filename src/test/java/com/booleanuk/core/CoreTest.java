0b52c7e4722f1889e0b35807a1b8f32bbbd4f530
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-16 10:17:17 +0100

    Created first test to check if the input is valid. Not sure if test is valid.

--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,5 +1,21 @@
 package com.booleanuk.core;
 
+import com.booleanuk.StudentMembers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 public class CoreTest {
+
+    @Test
+    public void testEmptyOrFalseInputs(){
+        StudentMembers students = new StudentMembers();
+        Scanner input = new Scanner(System.in);
+        students.run();
+        if (input.next().equals("")){
+            Assertions.assertEquals("Please use valid syntax", students.run(input));
+        }
+    }
 
 }
